64a2bbf9182831757a7f8482a9afed08e1d78df1
Regan-John Daniels <reganjohndaniels@gmail.com>
2022-07-25 15:00:13 +0200

    updates

--- data_exploration.py
+++ data_exploration.py
@@ -78,10 +78,6 @@ def date_delta(date:any, date_format, delta):
 def month_list(start, end, format):
     return pd.date_range(start, end, freq='MS').strftime(format).tolist()
 
-def create_dir(dir, file, type):
+def create_dir(dir:str, file:str):
     dir = dir.replace(r"\\", r"\\") + "\\" + file
-    if type == 'csv':
-        dir = dir + ".csv"
-    elif type == 'excel':
-        dir = dir + '.xlsx'
     return dir
